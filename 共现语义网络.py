9d9202dbd23606d78d49b4cb371103e8fbf8dca8
v_yuhaozeng(曾宇皓) <v_yuhaozeng@tencent.com>
2023-09-12 15:23:15 +0800

    项目更新

--- 共现语义网络.py
+++ 共现语义网络.py
@@ -13,108 +13,122 @@ import jieba
 
 
 def main():
-    df = pd.read_csv('./data/new_data.csv')
-
-    def fenci():
-        f = open('./共现语义/fenci.txt', 'w', encoding='utf-8-sig')
-        for line in df['分词']:
-            line = line.strip('\n')
-            # 停用词过滤
-            seg_list = jieba.cut(line, cut_all=False)
-            cut_words = (" ".join(seg_list))
-
-            # 计算关键词
-            all_words = cut_words.split()
-            c = Counter()
-            for x in all_words:
-                if len(x) >= 2 and x != '\r\n' and x != '\n':
-                    if is_all_chinese(x) == True and x not in stop_words:
-                        c[x] += 1
-            output = ""
-            for (k, v) in c.most_common(30):
-                output += k + " "
-
-            f.write(output + "\n")
-        else:
-            f.close()
-
-    fenci()
-
-    word = [] #记录关键词
-    f = open("./共现语义/fenci.txt", encoding='utf-8')
-    line = f.readline()
-    while line:
-        #print line
-        line = line.replace("\n", "") #过滤换行
-        line = line.strip('\n')
-        for n in line.split(' '):
-            #print n
-            if n not in word:
-                word.append(n)
-        line = f.readline()
-    f.close()
-
-    word_vector = coo_matrix((len(word),len(word)), dtype=np.int8).toarray()
-    print(word_vector.shape)
-
-    f = open("./共现语义/fenci.txt", encoding='utf-8')
-    line = f.readline()
-    while line:
-        line = line.replace("\n", "")  # 过滤换行
-        line = line.strip('\n')  # 过滤换行
-        nums = line.split(' ')
-        # 循环遍历关键词所在位置 设置word_vector计数
-        i = 0
-        j = 0
-        while i < len(nums):  # ABCD共现 AB AC AD BC BD CD加1
-            j = i + 1
-            w1 = nums[i]  # 第一个单词
-            while j < len(nums):
-                w2 = nums[j]  # 第二个单词
-                # 从word数组中找到单词对应的下标
-                k = 0
-                n1 = 0
-                while k < len(word):
-                    if w1 == word[k]:
-                        n1 = k
-                        break
-                    k = k + 1
-                # 寻找第二个关键字位置
-                k = 0
-                n2 = 0
-                while k < len(word):
-                    if w2 == word[k]:
-                        n2 = k
-                        break
-                    k = k + 1
-
-                # 重点: 词频矩阵赋值 只计算上三角
-                if n1 <= n2:
-                    word_vector[n1][n2] = word_vector[n1][n2] + 1
-                else:
-                    word_vector[n2][n1] = word_vector[n2][n1] + 1
-                j = j + 1
-            i = i + 1
-        # 读取新内容
-        line = f.readline()
-    f.close()
-
-    words = codecs.open("./共现语义/word_node_1.txt", "w", "utf-8")
-    i = 0
-    while i < len(word):
-        student1 = word[i]
-        j = i + 1
-        while j < len(word):
-            student2 = word[j]
-            if word_vector[i][j]>0:
-                words.write(student1 + " " + student2 + " "
-                    + str(word_vector[i][j]) + "\r\n")
-            j = j + 1
-        i = i + 1
-    words.close()
+    df = pd.read_csv('new_data.csv')
+    #
+    # # 导入停用词列表
+    # stop_words = []
+    # with open("stopwords_cn.txt", 'r', encoding='utf-8') as f:
+    #     lines = f.readlines()
+    #     for line in lines:
+    #         stop_words.append(line.strip())
+    #
+    # # 判断是否为中文
+    # def is_all_chinese(strs):
+    #     for _char in strs:
+    #         if not '\u4e00' <= _char <= '\u9fa5':
+    #             return False
+    #     return True
+    #
+    # def fenci():
+    #     f = open('fenci.txt', 'w', encoding='utf-8-sig')
+    #     for line in df['分词']:
+    #         line = line.strip('\n')
+    #         # 停用词过滤
+    #         seg_list = jieba.cut(line, cut_all=False)
+    #         cut_words = (" ".join(seg_list))
+    #
+    #         # 计算关键词
+    #         all_words = cut_words.split()
+    #         c = Counter()
+    #         for x in all_words:
+    #             if len(x) >= 2 and x != '\r\n' and x != '\n':
+    #                 if is_all_chinese(x) == True and x not in stop_words:
+    #                     c[x] += 1
+    #         output = ""
+    #         for (k, v) in c.most_common(30):
+    #             output += k + " "
+    #
+    #         f.write(output + "\n")
+    #     else:
+    #         f.close()
+    #
+    # fenci()
+    #
+    # word = [] #记录关键词
+    # f = open("fenci.txt", encoding='utf-8')
+    # line = f.readline()
+    # while line:
+    #     #print line
+    #     line = line.replace("\n", "") #过滤换行
+    #     line = line.strip('\n')
+    #     for n in line.split(' '):
+    #         #print n
+    #         if n not in word:
+    #             word.append(n)
+    #     line = f.readline()
+    # f.close()
+    #
+    # word_vector = coo_matrix((len(word),len(word)), dtype=np.int8).toarray()
+    # print(word_vector.shape)
+    #
+    # f = open("fenci.txt", encoding='utf-8')
+    # line = f.readline()
+    # while line:
+    #     line = line.replace("\n", "")  # 过滤换行
+    #     line = line.strip('\n')  # 过滤换行
+    #     nums = line.split(' ')
+    #     # 循环遍历关键词所在位置 设置word_vector计数
+    #     i = 0
+    #     j = 0
+    #     while i < len(nums):  # ABCD共现 AB AC AD BC BD CD加1
+    #         j = i + 1
+    #         w1 = nums[i]  # 第一个单词
+    #         while j < len(nums):
+    #             w2 = nums[j]  # 第二个单词
+    #             # 从word数组中找到单词对应的下标
+    #             k = 0
+    #             n1 = 0
+    #             while k < len(word):
+    #                 if w1 == word[k]:
+    #                     n1 = k
+    #                     break
+    #                 k = k + 1
+    #             # 寻找第二个关键字位置
+    #             k = 0
+    #             n2 = 0
+    #             while k < len(word):
+    #                 if w2 == word[k]:
+    #                     n2 = k
+    #                     break
+    #                 k = k + 1
+    #
+    #             # 重点: 词频矩阵赋值 只计算上三角
+    #             if n1 <= n2:
+    #                 word_vector[n1][n2] = word_vector[n1][n2] + 1
+    #             else:
+    #                 word_vector[n2][n1] = word_vector[n2][n1] + 1
+    #             j = j + 1
+    #         i = i + 1
+    #     # 读取新内容
+    #     line = f.readline()
+    # f.close()
+    #
+    # words = codecs.open("word_node_1.txt", "w", "utf-8")
+    # i = 0
+    # while i < len(word):
+    #     student1 = word[i]
+    #     j = i + 1
+    #     while j < len(word):
+    #         student2 = word[j]
+    #         if word_vector[i][j]>0:
+    #             words.write(student1 + " " + student2 + " "
+    #                 + str(word_vector[i][j]) + "\r\n")
+    #         j = j + 1
+    #     i = i + 1
+    # words.close()
 
     """ 第四步:图形生成 """
-    with open('./共现语义/word_node_1.txt','r',encoding='utf-8')as f:
+    with open('word_node_1.txt','r',encoding='utf-8')as f:
         content = f.readlines()
     list_word1 = []
     list_word2 = []
@@ -132,7 +146,7 @@ def main():
     df['weight'] = df['weight'].astype(int)
     df = df.sort_values(by=['weight'],ascending=False)
     df = df.dropna(how='any',axis=1)
-    new_df = df.iloc[0:150]
+    new_df = df.iloc[0:80]
 
     A = []
     B = []
@@ -150,17 +164,17 @@ def main():
     plt.rcParams['font.sans-serif'] = ['SimHei']
     plt.rcParams['font.family'] = 'sans-serif'
     #设置图像的大小，其中figsize参数设置图像的宽度和高度。
-    plt.figure(figsize=(20, 14))
+    plt.figure(figsize=(16, 9),dpi=500)
     #确定节点布局。这里使用了一种称为spring layout的布局算法，相当于在二维空间中对节点进行排列。iterations参数指定了进行节点排列的迭代次数。
     pos=nx.spring_layout(G,iterations=10)
     #绘制节点。其中alpha参数设置节点的透明度，node_size参数设置节点的大小。
-    nx.draw_networkx_nodes(G, pos, alpha=0.7,node_size=1600)
+    nx.draw_networkx_nodes(G, pos, alpha=0.7,node_size=800)
     #绘制边。其中width参数设置边的宽度，alpha参数设置边的透明度，edge_color参数设置边的颜色。
     nx.draw_networkx_edges(G,pos,width=0.5,alpha=0.8,edge_color='g')
     #添加标签。其中font_family参数指定图像中使用sans-serif字体族，alpha参数设置节点标签的透明度，font_size参数设置归纳节点标签的字体大小。
-    nx.draw_networkx_labels(G, pos, font_family='sans-serif', alpha=1,font_size='24')
-    plt.title("共现语义")
-    plt.savefig('./共现语义/共现语义.png')
+    nx.draw_networkx_labels(G, pos, font_family='sans-serif', alpha=1,font_size='10')
+    plt.title("co-occurrence semantics")
+    plt.savefig('共现语义.png')
     plt.show()
 
 
